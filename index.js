e62df79720ab87d0e74627b0708b3d19890fcc06
Yasi Moshtael <yasi.moshtael@gmail.com>
2021-06-11 15:44:04 -0700

    Created initial inquirer prompts for team manager based on assignment instructions

--- index.js
+++ index.js
@@ -1,2 +1,29 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
+
+const writeFile = (answers) => 
+``
+
+inquirer
+    .prompt ([
+        {
+            type: "input",
+            name: "name",
+            message: "What is the team manager's name?"
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "What is the team manager's ID?"
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is the team manager's email address?"
+        },
+        {
+            type: "input",
+            name: "office",
+            message: "What is the team manager's office number?"
+        },
+    ])
