7e2fe5e387afaac13dd3550d68864b2397c20114
Yasi Moshtael <yasi.moshtael@gmail.com>
2021-06-19 20:37:24 -0700

    Tried to create template literal for HTML to be generated and a funciton to generate the page, but it did not work. Coming back to this later

--- index.js
+++ index.js
@@ -51,8 +51,8 @@ const nextMember = () => {
         .prompt([
             {
                 type: "list",
-                message: "Let's finish building your team! Which type of employee would you like to add?",
-                choices: ["Engineer", "Intern", "I'm finished building my team."]
+                message: "Let's finish building your team! Which type of employee would you like to add next?",
+                choices: ["Engineer", "Intern", "I'm finished building my team."],
                 name: "employee"
             }
         ])
@@ -66,6 +66,10 @@ const nextMember = () => {
                 addIntern();
             } else {
                 // genereate HTML
+                const generateHTML = teamHTML(answers);
+                fs.writeFile("new.html", generateHTML, (err) =>
+                err ? console.log(err) : console.log("Congratulations! Your new Team Profile has been generated!")
+                );
             }
         })
 }
@@ -137,3 +141,53 @@ const addIntern = () => {
             nextMember();
         })
 }
+
+// HTML with provided answers
+const teamHTML = (answers) =>
+`
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style.css">
+    <title>Team Profile</title>
+</head>
+<body>
+    <header>
+        <h1>My Team</h1>
+    </header>
+
+    <main id="info-blocks">
+        <div class="row">
+            <div class="card">
+                <div class="card-head">
+                    <h3 class="name">${manager.getName()}</h3>
+                    <h4 class="role">${manager.getRole()}</h4>
+                </div>
+                <div class="card-info">
+                    <p class="id">ID: ${manager.getId()}</p>
+                    <a class="email">Email:  ${manager.getEmail()}</a>
+                    <p class="office">Office: ${manager.getOffice()}</p>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-head">
+                    <h3 class="name">${engineer.getName()}</h3>
+                    <h4 class="role">${engineer.getRole()}</h4>
+                </div>
+                <div class="card-info">
+                    <p class="id">ID: ${engineer.getId()}</p>
+                    <a class="email">Email:  ${engineer.getEmail()}</a>
+                    <p class="office">Office: ${engineer.getOffice()}</p>
+                </div>
+            </div>
+        </div>
+    </main>
+</body>
+</html>
+`
+
+// call ask manager to start the application
+addManager();
